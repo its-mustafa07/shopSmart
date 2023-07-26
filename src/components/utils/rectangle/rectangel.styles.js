b156204f0e9db03f0a6bf41fae3483142bd38a67
its-mustafa07 <atta.ul.mustafa.3@gmail.com>
2023-07-26 15:56:59 +0500

    implementing changes in header component

--- src/components/utils/rectangle/rectangel.styles.js
+++ src/components/utils/rectangle/rectangel.styles.js
@@ -1,14 +1,15 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
-  padding-top: 7rem;
+  display: flex;
+  align-items: center;
   padding-left: 3rem;
   padding-right: 1rem;
-  min-height: 27vh;
+  height: 7.5rem;
   width: 100vw;
   background-color: ${(props) => props.theme.color.primary};
-  border-bottom-left-radius: 6rem;
-  border-bottom-right-radius: 6rem;
+  border-bottom-right-radius: 1.5rem;
+  border-bottom-left-radius: 1.5rem;
   margin-bottom: ${(props) => props.marginBottom || 0};
 `;
 
@@ -16,28 +17,29 @@ export const FlexWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: inherit;
 `;
 
-export const LeftFlex = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.3rem;
-  font-size: 1.4rem;
-  color: ${({ theme }) => theme.color.semiWhite};
-`;
+// export const LeftFlex = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 1.3rem;
+//   font-size: 1.4rem;
+//   color: ${({ theme }) => theme.color.semiWhite};
+// `;
 
-export const Greetings = styled.p`
-  display: ${(props) => (props.greetings ? "block" : "none")};
-`;
+// export const Greetings = styled.p`
+//   display: ${(props) => (props.greetings ? "block" : "none")};
+// `;
 
-export const AvatarWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-`;
+// export const AvatarWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 2rem;
+// `;
 
-export const Avatar = styled.img`
-  height: 5rem;
-  width: 5rem;
-  border-radius: 50px;
-`;
+// export const Avatar = styled.img`
+//   height: 5rem;
+//   width: 5rem;
+//   border-radius: 50px;
+// `;
